c1ed1703c04d6c700cf39fd16d8649960ad1356c
JerryAlexLiang <1935990239@qq.com>
2017-11-30 23:18:40 +0800

    使用自定义View实现自定义的圆形进度条(progress) - Java代码控制属性设置

--- customview/src/main/java/com/example/customview/activity/MyProgressActivity.java
+++ customview/src/main/java/com/example/customview/activity/MyProgressActivity.java
@@ -1,9 +1,11 @@
 package com.example.customview.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.customview.R;
 import com.example.customview.customView.MyCustomProgress;
@@ -13,26 +15,63 @@ import com.example.customview.customView.MyCustomProgress;
  * 描述: 使用自定义View实现自定义圆形进度条的功能
  * 作者: liangyang
  */
-public class MyProgressActivity extends AppCompatActivity {
+public class MyProgressActivity extends AppCompatActivity implements View.OnClickListener {
 
     private MyCustomProgress myCustomProgress;
+    private boolean isChangeStyle = false;
+    private TextView progressValue;
+    private int currenProgress;
+    private Button buttonDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_progress);
         myCustomProgress = (MyCustomProgress) findViewById(R.id.my_progress);
-        Button button = (Button) findViewById(R.id.start_download);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //Java代码方式设置自定义progress的样式属性
+        myCustomProgress.setRoundWidth(10);
+        myCustomProgress.setRoundColor(Color.GREEN);
+        myCustomProgress.setProgressColor(Color.BLUE);
+        myCustomProgress.setTextColor(Color.RED);
+        myCustomProgress.setTextSize(70);
+
+        progressValue = (TextView) findViewById(R.id.get_progress_value);
+
+        Button buttonSetStyle = (Button) findViewById(R.id.progress_style);
+        buttonDownload = (Button) findViewById(R.id.start_download);
+
+        buttonSetStyle.setOnClickListener(this);
+        buttonDownload.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_download:
+                buttonDownload.setEnabled(false);
+                buttonDownload.setText("正在下载中...");
                 //模拟下载进度
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for (int i = 0; i <= 360; i++) {
+                        for (int i = 0; i <= 100; i++) {
                             myCustomProgress.setProgress(i);
+                            currenProgress = myCustomProgress.getProgress();
+                            progressValue.postInvalidate();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressValue.setText(currenProgress + "%");
+                                    if (currenProgress == 100){
+                                        buttonDownload.setEnabled(true);
+                                        buttonDownload.setText("开始下载");
+                                    }
+                                }
+                            });
+
                             //模拟耗时操作
                             try {
                                 Thread.sleep(100);
@@ -42,10 +81,24 @@ public class MyProgressActivity extends AppCompatActivity {
                         }
                     }
                 }).start();
-            }
-        });
+                break;
 
+            case R.id.progress_style:
+                if (isChangeStyle) {
+                    myCustomProgress.setStroke(true);
+                    progressValue.setVisibility(View.GONE);
+                    isChangeStyle = false;
+                } else {
+                    myCustomProgress.setStroke(false);
+                    //设置在填充模式进度条下面显示进度
+                    progressValue.setVisibility(View.VISIBLE);
+                    isChangeStyle = true;
+                }
+                break;
 
+            default:
+                break;
+        }
     }
 }
 
--- customview/src/main/java/com/example/customview/customView/MyCustomProgress.java
+++ customview/src/main/java/com/example/customview/customView/MyCustomProgress.java
@@ -5,14 +5,16 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
+
+import static android.graphics.Paint.Style.STROKE;
 
 /**
  * 创建日期：2017/11/30 on 下午7:58
- * 描述:自定义Progress
+ * 描述:自定义Progress(默认设置为空心描边进度条)
  * 作者:yangliang
  */
 public class MyCustomProgress extends View {
@@ -24,11 +26,19 @@ public class MyCustomProgress extends View {
     //进度环的颜色
     private int progressColor = Color.RED;
     //圆环的宽度
-    private int roundWidth = 10;
+    private int roundWidth = 20;
     //当前进度
-    private int progress = 45;
+    private int progress = 0;
+    //最大进度值
+    private int maxProgress = 100;
     //设置样式(是否填充,true为stroke,false为fill)
     private boolean isStroke = true;
+    //圆环中是否显示文字（当外环样式为stroke时显示文字）
+    private boolean isTextDisplay = true;
+    //文字的颜色
+    private int textColor = Color.BLUE;
+    //文字的大小
+    private int textSize = 75;
 
 
     public MyCustomProgress(Context context) {
@@ -59,17 +69,37 @@ public class MyCustomProgress extends View {
         paint.setColor(roundColor);
         paint.setStrokeWidth(roundWidth);
         paint.setAntiAlias(true);
-        paint.setStyle(isStroke ? Paint.Style.STROKE : Paint.Style.FILL);
+        //isStroke : true,则Style.STROKE 填充样式改为描边,false:则Style.FILL 填充样式改为填充
+        paint.setStyle(isStroke ? STROKE : Paint.Style.FILL);
         //半径
+        //int center = getWidth() / 2;// 得到圆心的x坐标
         float radius = (Math.min(getWidth(), getHeight()) - roundWidth * 2) / 2;
         canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, paint);
 
         //画弧度
         paint.setColor(progressColor);
-        paint.setAntiAlias(true
-        );
+        paint.setAntiAlias(true);
         RectF oval = new RectF(getWidth() / 2 - radius, getHeight() / 2 - radius, getWidth() / 2 + radius, getHeight() / 2 + radius);
-        canvas.drawArc(oval, -90, progress, !isStroke, paint);
+//        canvas.drawArc(oval, -90, progress, !isStroke, paint);
+        //useCenter:是否有弧的两边，True，有两边，False，只有一条弧
+        canvas.drawArc(oval, -90, 360 * progress / maxProgress, !isStroke, paint);
+
+        //在圆心画文字(进度值)
+        paint.reset();
+        paint.setStrokeWidth(0);
+        paint.setAntiAlias(true);
+        paint.setColor(textColor);
+        paint.setTextSize(textSize);
+        paint.setTypeface(Typeface.DEFAULT_BOLD);
+        // 中间的进度百分比，先转换成float在进行除法运算，不然都为0
+        int percent = (int) (((float) progress / (float) maxProgress) * 100);
+        float textWidth = paint.measureText(percent + "%");// 通过画笔测量文字的宽度(重点)
+        if (isTextDisplay && percent != 0 && isStroke) {
+            canvas.drawText(percent + "%", getWidth() / 2 - textWidth / 2, getWidth() / 2, paint);
+        }
+        if (isTextDisplay && percent == 100) {
+            canvas.drawText("完成", getWidth() / 2 - textWidth / 2, (getHeight() / 3) * 2, paint);
+        }
 
     }
 
@@ -78,7 +108,74 @@ public class MyCustomProgress extends View {
         this.progress = progress;
         //重点：改变进度要重画（这里的写法参考Android系统的ProgressBar的写法，其可以在子线程中更新UI）
         this.postInvalidate();//post到主线程更新
+    }
 
+    //封装属性，对外提供
+    public int getRoundColor() {
+        return roundColor;
+    }
 
+    public void setRoundColor(int roundColor) {
+        this.roundColor = roundColor;
+    }
+
+    public int getProgressColor() {
+        return progressColor;
+    }
+
+    public void setProgressColor(int progressColor) {
+        this.progressColor = progressColor;
+    }
+
+    public int getRoundWidth() {
+        return roundWidth;
+    }
+
+    public void setRoundWidth(int roundWidth) {
+        this.roundWidth = roundWidth;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public int getMaxProgress() {
+        return maxProgress;
+    }
+
+    public void setMaxProgress(int maxProgress) {
+        this.maxProgress = maxProgress;
+    }
+
+    public boolean isStroke() {
+        return isStroke;
+    }
+
+    public void setStroke(boolean stroke) {
+        isStroke = stroke;
+    }
+
+    public boolean isTextDisplay() {
+        return isTextDisplay;
+    }
+
+    public void setTextDisplay(boolean textDisplay) {
+        isTextDisplay = textDisplay;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public int getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(int textSize) {
+        this.textSize = textSize;
     }
 }
